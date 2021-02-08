76c5ff9912b803c7898db87ad1aead9b61f9732c
Larry Ross <lross@ev-k4.kzn.21-school.ru>
2021-02-08 23:27:55 +0300

    first commit

---
+++ ex01/ft_putstr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lross <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/08 18:00:02 by lross             #+#    #+#             */
+/*   Updated: 2021/02/08 22:19:20 by lross            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int		main(void)
+{
+	char	*str = "my name is jacob";
+
+	ft_putstr(str);
+}
